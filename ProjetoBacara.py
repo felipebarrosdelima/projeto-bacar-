d3ff444effcd9928c41779700516e44a2aef0ba9
joão Pedro Souza <joopedrosouzagg27@gmail.com>
2020-10-18 20:20:37 -0300

    regra avançada distribuição da terceira carta

--- ProjetoBacara.py
+++ ProjetoBacara.py
@@ -5,27 +5,27 @@
 import random
 import random
 
-q_jogadores=int(input('quantos jogadores tem no jogo ')) #Variável que guarda quantos jogadores vão estar no jogo
-lista_jogadores = [] # Lista que guarda respectivamente as fichas que cada jogador vai colocar no jogo, seu destino de aposta e por final o quanto é sua aposta
-contador_jogadores = 0 # Contador utilizado para o preenchimento da lista_jogadores
-contador_nome_jogador = 1 # Contador utilziado para o nome de cada jogador
-while contador_jogadores < q_jogadores: # Looping utilizado para criação da lista_jogadores
-    fichas = int(input(f'Digite quantas fichas o jogador {contador_nome_jogador} deseja apostar')) #variável utilizada para guardar as fichas que cada jogador entra no jogo
-    destino_aposta2 = input('Digite em quem você quer apostar? Opções: Banca, Empate, Jogador. ') #Varável utilizada para guardar o destino da aposta de cada jogador 
-    aposta = int(input('Digite o quanto você deseja apostar nessa rodada')) #Variável utilizada para guardar a aposta de cada jogador
-    lista_lista = [] # Lista  que guarda as informações anteriores
-    lista_lista.append(fichas)
-    while aposta > fichas: #looping utilizado para validar a aposta
-        aposta = int(input('Sua aposta não foi valida, por favor aposte de novo'))
+#q_jogadores=int(input('quantos jogadores tem no jogo ')) #Variável que guarda quantos jogadores vão estar no jogo
+#lista_jogadores = [] # Lista que guarda respectivamente as fichas que cada jogador vai colocar no jogo, seu destino de aposta e por final o quanto é sua aposta
+#contador_jogadores = 0 # Contador utilizado para o preenchimento da lista_jogadores
+#contador_nome_jogador = 1 # Contador utilziado para o nome de cada jogador
+#while contador_jogadores < q_jogadores: # Looping utilizado para criação da lista_jogadores
+    #fichas = int(input(f'Digite quantas fichas o jogador {contador_nome_jogador} deseja apostar')) #variável utilizada para guardar as fichas que cada jogador entra no jogo
+    #destino_aposta2 = input('Digite em quem você quer apostar? Opções: Banca, Empate, Jogador. ') #Varável utilizada para guardar o destino da aposta de cada jogador 
+    #aposta = int(input('Digite o quanto você deseja apostar nessa rodada')) #Variável utilizada para guardar a aposta de cada jogador
+    #lista_lista = [] # Lista  que guarda as informações anteriores
+    #lista_lista.append(fichas)
+    #while aposta > fichas: #looping utilizado para validar a aposta
+        #aposta = int(input('Sua aposta não foi valida, por favor aposte de novo'))
         
-    lista_lista.append(aposta) # Colocando na lista_lista valor da aposta
-    lista_lista.append(destino_aposta2) #Colocando na lista_lista o destino na aposta
-    lista_jogadores.append(lista_lista) #Colocando na list_jogadores os valores da lista_lista
-    contador_jogadores +=1
-    contador_nome_jogador +=1
+    #lista_lista.append(aposta) # Colocando na lista_lista valor da aposta
+    #lista_lista.append(destino_aposta2) #Colocando na lista_lista o destino na aposta
+    #lista_jogadores.append(lista_lista) #Colocando na list_jogadores os valores da lista_lista
+    #contador_jogadores +=1
+    #contador_nome_jogador +=1
 
-print(lista_jogadores)
-print(lista_jogadores[0][0])
+#print(lista_jogadores)
+#print(lista_jogadores[0][0])'''
 
 fj = (int(input('Digite quantas fichas você deseja colocar no jogo '))) #fj refere-se a variável que vai guardar quantas fichas o jogador vai colocar no jogo.
 
@@ -57,13 +57,39 @@ while fj > 0: #Looping utilizado para fazer com que o jogador continue jogando a
     if soma_jogador >= 10: # Condição para falar que se por exemplo a soma der 12 então o valor real é 2 etc...
         soma_jogador -= 10 
     if soma_banca >= 10: # Condição para falar que se por exemplo a soma der 12 então o valor real é 2 etc...
-        soma_banca -=10  
+        soma_banca -=10 
+
+  #edito      
+
+    if soma_banca != 6 and soma_banca < 6:#Se a soma das duas cartas do banco for igual a 6 ou mais, ele não recebe a terceira carta
+        if soma_banca <=5 and soma_jogador < 8 or soma_jogador > 9 : # Condição para distribuir a terceira carta
+            Mão_Banca.append(random.choice(Valores_cartas))
+            soma_banca = sum(Mão_Banca)
+
     if soma_jogador <=5 and soma_banca < 8 or soma_banca > 9: # Condição para distribuir a terceira carta
-        Mão_jogador.append(random.choice(Valores_cartas))
+        terceira_carta=random.choice(Valores_cartas)#valor da terceira carta
+        Mão_jogador.append(terceira_carta)
         soma_jogador = sum(Mão_jogador)
-    if soma_banca <=5 and soma_jogador < 8 or soma_jogador > 9 : # Condição para distribuir a terceira carta
-        Mão_Banca.append(random.choice(Valores_cartas))
-        soma_banca = sum(Mão_Banca)
+
+        if soma_banca<=5: # condição para que se o jogador receber uma terceira carta e a soma das duas cartas do banco for menor ou igual a 5, o banco recebe a terceira carta de acordo com a condições abaixo
+            if soma_banca==0:# condilção para se a soma da banca for igual a zero mais uma cata é distribuida independente da treceira carta do jogador
+                Mão_Banca.append(random.choice(Valores_cartas))
+            if soma_banca==1:#condilção para se a soma da banca for igual a 1 mais uma cata é distribuida independente da terceira carta do jogador
+                Mão_Banca.append(random.choice(Valores_cartas))
+            if soma_banca==2:#condilção para se a soma da banca for igual a 2 mais uma cata é distribuida independente da terceira carta do jogador
+                Mão_Banca.append(random.choice(Valores_cartas))
+            if soma_banca==3 and terceira_carta !=8:#condilção para se a soma da banca for igual a 3 mais uma cata é distribuida se a terceira carta do jogador for diferente de 8
+                Mão_Banca.append(random.choice(Valores_cartas))
+            if soma_banca==4 and terceira_carta>1 and terceira_carta<8:#condilção para se a soma da banca for igual a 4 mais uma cata é distribuida se a terceira carta do jogador não for 0, 1, 8, 9
+                Mão_Banca.append(random.choice(Valores_cartas))
+            if soma_banca==5 and terceira_carta>3 and terceira_carta<8:#condilção para se a soma da banca for igual a 5 mais uma cata é distribuida se a terceira carta do jogador não for 0, 1, 3, 8 e 9
+
+    else:# se o jogador não recebeu uma terceira carta e a soma das duas cartas do banco for menor ou igual a 5, ele recebe a terceira carta;
+        if soma_banca<=5:#soma das duas cartas do banco for menor ou igual a 5, ele recebe a terceira carta;
+            Mão_jogador.append(random.choice(Valores_cartas))
+            soma_jogador = sum(Mão_jogador)
+
+#edito
 
     if soma_jogador >= 10: # Condição para falar que se por exemplo a soma der 12 então o valor real é 2 etc...
         soma_jogador -=10
